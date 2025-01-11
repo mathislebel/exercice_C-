d850e19572340138490e23bebbbec091fcf5edec
Mathis LEBEL <145991354+mathislebel@users.noreply.github.com>
2025-01-11 16:19:21 +0100

    firts

---
+++ data_Structure/data.hpp
@@ -0,0 +1,12 @@
+/*
+For this exercise you will need to:
+Build a data type of your choice (class, struct), to represent one real-world set of entities (e.g. Students, Cars, Products, etc.). Think of appropriate attributes in each case (e.g. year of enrolment, name, etc.)
+Create instances/objects of your data type and store them in a Data Structure of your choice (e.g. list, vector, BST). Optimally, the data structure should follow your own implementation. Data can be randomly generated.
+Implement addition, retrieval, updating and removal functionalities, through a simple console menu.
+Implement a version of Merge Sort, to sort your data structure based on a "sortable" attribute (e.g. year of enrolment, name)
+Support any function that depends on search (retrieval, updating and removal), with your implementation of Binary Search (or, optionally, a suitable tree structure).
+Maintain a public remote repository on GitHub, with just the necessary documentation on how to build and run your code. Include a short description about what your project does and what limitations it may have (bugs, unfinished tasks, etc.).
+*/
+
+// linking librairy
+
