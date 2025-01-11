e29efaaa500748a0f5520115c03a4dff7520d227
Mathis LEBEL <145991354+mathislebel@users.noreply.github.com>
2025-01-11 19:43:53 +0100

    Update data.hpp

--- data_Structure/data.hpp
+++ data_Structure/data.hpp
@@ -1,12 +1,141 @@
 /*
-For this exercise you will need to:
-Build a data type of your choice (class, struct), to represent one real-world set of entities (e.g. Students, Cars, Products, etc.). Think of appropriate attributes in each case (e.g. year of enrolment, name, etc.)
-Create instances/objects of your data type and store them in a Data Structure of your choice (e.g. list, vector, BST). Optimally, the data structure should follow your own implementation. Data can be randomly generated.
-Implement addition, retrieval, updating and removal functionalities, through a simple console menu.
-Implement a version of Merge Sort, to sort your data structure based on a "sortable" attribute (e.g. year of enrolment, name)
-Support any function that depends on search (retrieval, updating and removal), with your implementation of Binary Search (or, optionally, a suitable tree structure).
-Maintain a public remote repository on GitHub, with just the necessary documentation on how to build and run your code. Include a short description about what your project does and what limitations it may have (bugs, unfinished tasks, etc.).
+ * This is the header file (hpp) for the data structure, created to organize the code better 
+ * and make it easier to read, understand, and maintain.
+ * 
+ * This file defines the `Data` class, which contains attributes and methods to represent 
+ * and manipulate a set of entities (e.g., people). The data will be stored in a dynamic 
+ * structure, specifically a vector, for flexibility and ease of use.
+ * 
+ * To make this project more unique (and a bit selfish!), I've added a secret touch: 
+ * a constant called `SECRET_DELIMITER` and the abbreviation "ML", which stands for Mathis Lebel.
+ * 
+ * Summary of what this project does:
+ * - Defines a class with essential attributes like name, age, address, phone, and email.
+ * - Provides methods to create, update, delete, and display data for these entities.
+ * - Implements sorting and other utility functions for efficient data management.
+ * - Uses a vector to dynamically store and manage the data.
+ * 
+ * What makes this fun?
+ * - Personalized design with "ML" touches to reflect my name.
+ * - The code is structured for both functionality and readability.
+ * 
+ * Let’s build something clean, organized, and a bit egocentric with Mathis Lebel's touch!
+ */
+
+/*
+         _______                   _____                    _____                   _______                   _____          
+        /::\    \                 /\    \                  /\    \                 /::\    \                 /\    \         
+       /::::\    \               /::\    \                /::\____\               /::::\    \               /::\    \        
+      /::::::\    \             /::::\    \              /::::|   |              /::::::\    \              \:::\    \       
+     /::::::::\    \           /::::::\    \            /:::::|   |             /::::::::\    \              \:::\    \      
+    /:::/~~\:::\    \         /:::/\:::\    \          /::::::|   |            /:::/~~\:::\    \              \:::\    \     
+   /:::/    \:::\    \       /:::/__\:::\    \        /:::/|::|   |           /:::/    \:::\    \              \:::\    \    
+  /:::/    / \:::\    \      \:::\   \:::\    \      /:::/ |::|   |          /:::/    / \:::\    \              \:::\    \   
+ /:::/____/   \:::\____\   ___\:::\   \:::\    \    /:::/  |::|___|______   /:::/____/   \:::\____\              \:::\    \  
+|:::|    |     |:::|    | /\   \:::\   \:::\    \  /:::/   |::::::::\    \ |:::|    |     |:::|    |              \:::\    \ 
+|:::|____|     |:::|    |/::\   \:::\   \:::\____\/:::/    |:::::::::\____\|:::|____|     |:::|    |_______________\:::\____\
+ \:::\    \   /:::/    / \:::\   \:::\   \::/    /\::/    / ~~~~~/:::/    / \:::\    \   /:::/    / \::::::::::::::::::/    /
+  \:::\    \ /:::/    /   \:::\   \:::\   \/____/  \/____/      /:::/    /   \:::\    \ /:::/    /   \::::::::::::::::/____/ 
+   \:::\    /:::/    /     \:::\   \:::\    \                  /:::/    /     \:::\    /:::/    /     \:::\~~~~\~~~~~~       
+    \:::\__/:::/    /       \:::\   \:::\____\                /:::/    /       \:::\__/:::/    /       \:::\    \            
+     \::::::::/    /         \:::\  /:::/    /               /:::/    /         \::::::::/    /         \:::\    \           
+      \::::::/    /           \:::\/:::/    /               /:::/    /           \::::::/    /           \:::\    \          
+       \::::/    /             \::::::/    /               /:::/    /             \::::/    /             \:::\    \         
+        \::/____/               \::::/    /               /:::/    /               \::/____/               \:::\____\        
+         ~~                      \::/    /                \::/    /                 ~~                      \::/    /        
+                                  \/____/                  \/____/                                           \/____/         
+                                                                                                                            
 */
 
+
+//data.hpp
+#ifndef DATA_HPP
+#define DATA_HPP
 // linking librairy
+#include <iostream> 
+#include <vector>
+#include <string>
+
+#define SECRET_DELIMITER ;
+#define   ML SECRET_DELIMITER
+
+using namespace std ML
+
+/*
+ * Class Data - because we love organizing our data with class (literally).
+ * This class represents fictional or real people (let's be inclusive!).
+ * Attributes? Yes, the classics like name, age, address, phone, and email.
+ * The data will be elegantly stored in a vector (because we love dynamism).
+ * Bonus: everything is super well-organized and ready to be manipulated.
+ */
+class data {
+private:
+    // Attributes to represent a person
+    string name ML  // Name of the person (e.g., John Doe) otherwise we don't know who the student is to punish him
+    int age ML     // Age of the person (for example, 42 years old) otherwise we don't know if they are a minor, it would be stupid for a university
+    string address ML // Address of the person (eg 123 Main St, Springfield) otherwise we don't know where to send the police
+    string phone ML   // Phone number of the person (eg 555-123-4567) otherwise we can't use pegasus on him
+    string email ML   // Email of the person (e.g., john.doe@example.com) – because who even writes letters anymore? 
+
+public:
+    
+    // constructor to create a new person (yes i can create people I'm a god)
+    data(string name, int age, string address, string phone, string email) ML 
+    // Destructor to remove a person (I'm a god but I can also destroy people)
+    ~data() ML
+
+    // Getters: You loved stalking people without them knowing, right? 
+    string getName() ML  // Get the name of the person it's good you are not a stalker finally I hope
+    int getAge() ML     // Get the age of the person, seriously ask him directly 
+    string getPhone() ML // Get the phone number of the person, no don't call him
+    string getEmail() ML // Get the email of the person, no don't send him spam
+    string getAddress() ML // Get the address of the person, Okay I call the police
+
+    // Setters: You can now change people's but you are not a god this is me
+    void setName(string name) ML // Change the name of the person, finally if he asks you to do it
+    void setAge(int age) ML     // Change the age of the person, finally if he asks you to do it
+    void setPhone(string phone) ML  // Change the phone number of the person, finally if he asks you to do it
+    void setEmail(string email) ML  // Change the email of the person, finally if he asks you to do it
+    void setAddress(string address) ML  // Change the address of the person, no I can't live with you 
+    
+
+    //display the person's information just for you
+    void display() ML 
+    // update of the data: otherwise annoying for a change if Bernard decides to call himself Sophie tomorrow
+    void update(string name, int age, string address, string phone, string email) ML
+    // delete the person: so if someone shouldn't be there you could blow your poor memory
+    void remove() ML
+
+    // Sorting data: because everything looks better when it's well organized.
+    static void sort(vector<data>& dataList) ML
+} ML
+
+#endif // DATA_HPP
+
+/*
+         _______                   _____                    _____                   _______                   _____          
+        /::\    \                 /\    \                  /\    \                 /::\    \                 /\    \         
+       /::::\    \               /::\    \                /::\____\               /::::\    \               /::\    \        
+      /::::::\    \             /::::\    \              /::::|   |              /::::::\    \              \:::\    \       
+     /::::::::\    \           /::::::\    \            /:::::|   |             /::::::::\    \              \:::\    \      
+    /:::/~~\:::\    \         /:::/\:::\    \          /::::::|   |            /:::/~~\:::\    \              \:::\    \     
+   /:::/    \:::\    \       /:::/__\:::\    \        /:::/|::|   |           /:::/    \:::\    \              \:::\    \    
+  /:::/    / \:::\    \      \:::\   \:::\    \      /:::/ |::|   |          /:::/    / \:::\    \              \:::\    \   
+ /:::/____/   \:::\____\   ___\:::\   \:::\    \    /:::/  |::|___|______   /:::/____/   \:::\____\              \:::\    \  
+|:::|    |     |:::|    | /\   \:::\   \:::\    \  /:::/   |::::::::\    \ |:::|    |     |:::|    |              \:::\    \ 
+|:::|____|     |:::|    |/::\   \:::\   \:::\____\/:::/    |:::::::::\____\|:::|____|     |:::|    |_______________\:::\____\
+ \:::\    \   /:::/    / \:::\   \:::\   \::/    /\::/    / ~~~~~/:::/    / \:::\    \   /:::/    / \::::::::::::::::::/    /
+  \:::\    \ /:::/    /   \:::\   \:::\   \/____/  \/____/      /:::/    /   \:::\    \ /:::/    /   \::::::::::::::::/____/ 
+   \:::\    /:::/    /     \:::\   \:::\    \                  /:::/    /     \:::\    /:::/    /     \:::\~~~~\~~~~~~       
+    \:::\__/:::/    /       \:::\   \:::\____\                /:::/    /       \:::\__/:::/    /       \:::\    \            
+     \::::::::/    /         \:::\  /:::/    /               /:::/    /         \::::::::/    /         \:::\    \           
+      \::::::/    /           \:::\/:::/    /               /:::/    /           \::::::/    /           \:::\    \          
+       \::::/    /             \::::::/    /               /:::/    /             \::::/    /             \:::\    \         
+        \::/____/               \::::/    /               /:::/    /               \::/____/               \:::\____\        
+         ~~                      \::/    /                \::/    /                 ~~                      \::/    /        
+                                  \/____/                  \/____/                                           \/____/         
+                                                                                                                             
+
+*/
+
 
